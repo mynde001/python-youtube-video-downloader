1281cffcd4eca2f201464f813bab3873608502b7
Mindaugas <mindaugas.antanas.grigaitis@gmail.com>
2024-09-01 00:36:28 +0300

    Wrapped the whole thing into a function

--- app.py
+++ app.py
@@ -8,85 +8,88 @@ from yt_dlp.utils import download_range_func
 # p - Last URL
 # f - Download full video
 
-last_url = ""
+def main():
+    last_url = ""
 
-while 1:
-    try:
-        yt_url = input("Enter a YouTube video URL: ")
+    while 1:
+        try:
+            yt_url = input("Enter a YouTube video URL: ")
 
-        if yt_url == "q":
-            break
+            if yt_url == "q":
+                break
 
-        if yt_url == "p" and last_url != "":
-            yt_url = last_url
-        elif yt_url == "p" and last_url == "":
-            print("No known last URL")
-            continue
+            if yt_url == "p" and last_url != "":
+                yt_url = last_url
+            elif yt_url == "p" and last_url == "":
+                print("No known last URL")
+                continue
 
-        last_url = yt_url
+            last_url = yt_url
 
-        video_starts = input("Video starts at (0:0:0): ")
+            video_starts = input("Video starts at (0:0:0): ")
 
-        if video_starts == "q":
-            break
-        elif video_starts == "r":
-            continue
+            if video_starts == "q":
+                break
+            elif video_starts == "r":
+                continue
 
-        video_ends = ""
+            video_ends = ""
 
-        if video_starts != "f":
-            video_ends = input("Video ends at (0:0:0): ")
-        else:
-            video_starts = "0"
-            video_ends = "0"
-
-        if video_ends == "q":
-            break
-        elif video_ends == "r":
-            continue
-        elif video_ends == "f":
-            video_starts = "0"
-            video_ends = "0"
-
-        split_video_start = video_starts.split(":")
-        split_video_end = video_ends.split(":")
-
-        hours = 0
-        minutes = 0
-        seconds = 0
-
-        start_time = 0
-        end_time = 0
-
-        for idx, val in enumerate((split_video_start, split_video_end)):
-            if len(val) == 3:
-                hours = int(val[0]) * 60 * 60
-                minutes = int(val[1]) * 60
-                seconds = int(val[2])
-            elif len(val) == 2:
-                minutes = int(val[0]) * 60
-                seconds = int(val[1])
+            if video_starts != "f":
+                video_ends = input("Video ends at (0:0:0): ")
             else:
-                seconds = int(val[0])
+                video_starts = "0"
+                video_ends = "0"
 
-            if idx == 0:
-                start_time = hours + minutes + seconds
-            else:
-                end_time = hours + minutes + seconds
+            if video_ends == "q":
+                break
+            elif video_ends == "r":
+                continue
+            elif video_ends == "f":
+                video_starts = "0"
+                video_ends = "0"
+
+            split_video_start = video_starts.split(":")
+            split_video_end = video_ends.split(":")
+
+            hours = 0
+            minutes = 0
+            seconds = 0
+
+            start_time = 0
+            end_time = 0
+
+            for idx, val in enumerate((split_video_start, split_video_end)):
+                if len(val) == 3:
+                    hours = int(val[0]) * 60 * 60
+                    minutes = int(val[1]) * 60
+                    seconds = int(val[2])
+                elif len(val) == 2:
+                    minutes = int(val[0]) * 60
+                    seconds = int(val[1])
+                else:
+                    seconds = int(val[0])
+
+                if idx == 0:
+                    start_time = hours + minutes + seconds
+                else:
+                    end_time = hours + minutes + seconds
 
 
-        ydl_opts = {
-            "format": "mp4",
-            # "format": "bestvideo+bestaudio/best",
-            "ffmpeg_location": f"{os.getenv('USERPROFILE')}/Desktop/YT-DL/bin",
-            "download_ranges": download_range_func(None, [(start_time, end_time)]),
-            "force_keyframes_at_cuts": True,
-            "overwrites": True,
-            "outtmpl": f"{os.getenv('USERPROFILE')}/Downloads/%(title)s.%(ext)s"
-        }
+            ydl_opts = {
+                "format": "mp4",
+                # "format": "bestvideo+bestaudio/best",
+                "ffmpeg_location": f"{os.getenv('USERPROFILE')}/Desktop/YT-DL/bin",
+                "download_ranges": download_range_func(None, [(start_time, end_time)]),
+                "force_keyframes_at_cuts": True,
+                "overwrites": True,
+                "outtmpl": f"{os.getenv('USERPROFILE')}/Downloads/%(title)s.%(ext)s"
+            }
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(yt_url)
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download(yt_url)
 
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
+
+main()
